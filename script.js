8f677b1a0d5dc9eeb391c61d123693dea642ca07
Suzane Feitosa <suzanefeitosa08@icloud.com>
2023-01-23 20:21:19 -0300

    add script, adjustment html and css

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+    gym: ["02-01", "02-02", "02-03", "02-03", "02-04","02-05" ,"02-06" ,"02-07" ,"02-08"],
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
