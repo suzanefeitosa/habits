46956e83699c1bc2d239176092ca0e71d4c27585
Suzane Feitosa <suzanefeitosa08@icloud.com>
2023-01-23 20:27:49 -0300

    add background

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const form = document.querySelector('#form-habits')
-const nlwSetup = new NLWSetup(form)
+const form = document.querySelector("#form-habits");
+const nlwSetup = new NLWSetup(form);
 
 const data = {
-    gym: ["02-01", "02-02", "02-03", "02-03", "02-04","02-05" ,"02-06" ,"02-07" ,"02-08"],
-}
+  gym: [
+    "02-01",
+    "02-02",
+    "02-03",
+    "02-03",
+    "02-04",
+    "02-05",
+    "02-06",
+    "02-07",
+    "02-08",
+  ],
+};
 
-nlwSetup.setData(data)
-nlwSetup.load()
+nlwSetup.setData(data);
+nlwSetup.load();
